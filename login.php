2ecae3cd05166ebafe909b817ab9e1d23b396065
bello harissa <belloharissa@gmail.com>
2023-04-26 17:08:14 +0200

    first commit

---
+++ login.php
@@ -0,0 +1,106 @@
+<?php 
+
+session_start();
+if (isset($_SESSION['id'])) {
+  
+  header("Location: formPublication.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css3/styles.css">
+    <title>connexion</title>
+</head>
+<body>
+    <div class="wrapper">
+      <section class="form login">
+        <header>CONNEXION</header>
+        <form  method="POST" id="forms" enctype="multipart/form-data" autocomplete="off">
+          <div class="error-text"></div>
+          
+          <div class="field input">
+            <label>Email</label>
+            <input type="objet" name="emai" id="email" placeholder="Entrer votre email" required>
+          </div>
+
+          <div class="field input">
+            <label>mot de passe</label>
+            <input type="password" name="pwde" id="pwd" placeholder="Entrer votre mot de passe" required>
+          </div>
+          <div id="res" style="color:red;"></div>
+          <div id="res1" style="color: red"></div>
+
+          <div class="field button" >
+            <input type="submit" id="connexion" name="submit" value="Connexion" style="background-color: #098d32">
+          </div>
+         <p>si vous n'avez pas un compte<a href="inscription.php" style="color: #098d32"> cliquez ici</a></p>
+        </form>
+      </section>
+    </div>
+    
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/connexion.js"></script>
+  
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script>
+    $(document).ready(function(){       
+        $("#connexion").click(function(e){
+            e.preventDefault();
+            let emptyInputCount=0;
+            
+            $("#forms input").each(function(){
+                var input = $(this);
+                if(input.val() == ''){
+                    input.css('border-color','red');
+                    $('#res').html("Veuillez remplir correctement les champs !");  
+                    emptyInputCount = 1;
+                }
+                else{
+                    input.css('border-color','#ced4da');
+                }
+            });
+
+            if(emptyInputCount == 0){
+                let getEmail = $("#email").val();
+                let getmotDePasse = $("#pwd").val();
+              
+                postObj = {
+                    emai: getEmail,
+                    pwde: getmotDePasse,
+                }
+                $.ajax({
+                    type: 'POST',
+                    url:'verifie.php',
+                    data:postObj,
+                    success: function(data){
+                        parseJson = JSON.parse(data);
+
+                        if(parseJson.success_msg)
+                        {
+                          window.location.href="formPublication.php";
+                          document.getElementById('signup-form').reset();
+                        }
+                        else{
+                            if (parseJson.suces_msg) {
+                                $('#res1').html("Veuillez vérifier votre email ou mot de passe!");   
+                            }
+                        }
+         
+                    }
+
+                });
+			// }
+		}
+
+        });
+    });
+</script>
+  </body>
+  </html>
---
+++ verifie.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include('connect.php');
+
+if(isset($_POST["emai"])){
+
+$email = $_POST["emai"];
+$motDePasse = md5(addslashes($_POST["pwde"]));
+
+// @$roles = 0;
+
+    $sql="SELECT * FROM usser WHERE emails ='$email' AND motPasses ='$motDePasse'";
+    // $rss = mysqli_query($conn,$sql);
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      $row = $result->fetch_assoc();
+      $id_session = $row['id'];
+      $_SESSION["id"] = $id_session;
+      echo json_encode(['success_msg' => 'User a été enregistrer']);
+  } 
+  else {
+    echo json_encode(['suces_msg' => 'User n a pas été enregistrer']);
+}
+
+  $conn->close();
+}
+
+
+?>
